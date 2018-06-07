dda4f963d88bb52b3eba0efa1123acec6427f0b1
michael <michaeltilman77@gmail.com>
2018-06-07 20:34:05 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop(array){
+  for(i = 0; i < 25 ; i++){
+    if(i === 0){
+      array.push("I am " + i+1 + " strange loop");
+    } else {
+      array.push("I am " + i+1 + " strange loops");
+    }
+  }
+}
+function whileLoop(n){
+  while(n > 0){
+    console.log(n);
+    n--;
+  }
+}
+function doWhileLoop(array){
+  do{
+    array.pop();
+  } while(
+    array.length>0)
+}
